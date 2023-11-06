af6fe06052d7d2313041f66a97b7b91b88514693
Thor <thor.schott@gmail.com>
2023-11-06 12:34:17 +0100

    A2 list.component: html+ts; app.component.html, gelöst bis inkl. Extra-Bonus-Aufgabe

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -10,8 +10,31 @@ export class ListComponent {
   newTodo: string = '';
   offeneTodos: number = 0;
 
+  data = [
+    {task: "HTML", done: true},
+    {task: "CSS", done: true},
+    {task: "JavaScript", done: true},
+    {task: "Angular", done: false}
+  ];
+
+  constructor() {this.countOffeneTodos()}
+
   countOffeneTodos(){
-    this.offeneTodos++;
+    let counter: number = 0;
+    for(let item of this.data){
+      if( ! item.done){
+        counter++;
+      }
+    }
+    this.offeneTodos = counter;
+  }
+
+  addTodo(){
+    if( this.newTodo.trim() != ''){
+      this.data.push({task: this.newTodo, done: false});
+      this.countOffeneTodos();
+      this.newTodo = '';
+    }
   }
 
 }
